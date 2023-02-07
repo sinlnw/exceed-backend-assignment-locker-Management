5b3421c4e600b30cbcd3899dbff6b2c540a49a97
sinlnw <sinlnw4567@gmail.com>
2023-02-07 17:03:55 +0700

    hide info

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ DATABASE_NAME = "exceed07"
 COLLECTION_NAME = "locker_man"
 MONGO_DB_URL = f"mongodb://******:*****@mongo.*********.online"   # put your own URL
 MONGO_DB_PORT = 8443
-MONGO_DB_URL = f"mongodb://exceed07:8td6VF6w@mongo.exceed19.online"   # put your own URL
 
 client = MongoClient(f"{MONGO_DB_URL}:{MONGO_DB_PORT}/?authMechanism=DEFAULT")
 
